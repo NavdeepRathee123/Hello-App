ee4d0763c53e94a4b7809b7148d0a022da25d2c7
Navdeep Rathee <navdeeprathee12@gmail.com>
2026-03-25 09:31:18 +0530

    Feature UC1 Created Successfully

---
+++ src/main/java/com/HelloAppx/HelloApp.java
@@ -0,0 +1,10 @@
+package com.HelloAppx;
+import java.util.*;
+public class HelloApp
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Hello World !");
+    }
+}
